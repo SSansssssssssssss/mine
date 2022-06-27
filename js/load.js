2c2ae12192d215e2c4be5f1cf2a0de7840e6c4c2
SSansssssssssssss <99086651+SSansssssssssssss@users.noreply.github.com>
2022-06-27 13:48:14 +0800

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,3 +1,8 @@
 depth = localStorage.getItem("depthsave")
 depthcounter = localStorage.getItem("depthcountersave")
 level = localStorage.getItem("levelsave")
+function reset() {
+ depth = 0
+ depthcounter = 1
+ level = 1
+}
