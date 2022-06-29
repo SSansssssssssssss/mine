4c368d38ec8a746a0f03896fcd9eacfe2693e80f
SSansssssssssssss <99086651+SSansssssssssssss@users.noreply.github.com>
2022-06-29 18:02:44 +0800

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,6 +1,6 @@
 function save() {
- localStorage.setItem("depthsave", depth)
- localStorage.setItem("depthcountersave", depthcounter)
- localStorage.setItem("levelsave", level)
+ window.localStorage.setItem("depthsave", depth)
+ window.localStorage.setItem("depthcountersave", depthcounter)
+ window.localStorage.setItem("levelsave", level)
 }
 setInterval(save, 1)
