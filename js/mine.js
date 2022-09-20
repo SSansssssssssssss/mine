34dee858a77ec0b32fb91ee9402be3b189682c10
SSansssssssssssss <99086651+SSansssssssssssss@users.noreply.github.com>
2022-09-20 22:49:03 +0800

    Update mine.js

--- js/mine.js
+++ js/mine.js
@@ -2,5 +2,5 @@
 document.getElementById("mine").innerHTML = depth; // writes depth
 function mine() {
  depth = depth + (depthcounter * level); // adds depth     
- document.getElementById("mine").innerHTML = depth; // writes depth
+ document.getElementById("mine").innerHTML = depth + sign; // writes depth
 }
