d4261c5992a3e3ecca63a6c3d9de106cd3e4aa20
SSansssssssssssss <99086651+SSansssssssssssss@users.noreply.github.com>
2022-09-20 23:22:37 +0800

    Update percent.js

--- js/percent.js
+++ js/percent.js
@@ -1,8 +1,21 @@
-
+punlocked = 0
 function called() {
-	p = depth / 100;
-	str = " "
-	p2 = str + p
-	document.getElementById("percent").innerHTML = p2 + "%"; // writes depth
+ if (punlocked = 0) {
+  p = depth / 100;
+  str = " "
+  p2 = str + p
+  document.getElementById("percent").innerHTML = p2 + "%"; // writes depth
+  document.getElementById("name").innerHTML = "Get 1K depth for pickaxes!";
+  if (depth > 10000 && punlocked == 0) {
+   punlocked = 1
+  }
+ }
+ else {
+  p = depth / 10000000;
+  str = " "
+  p2 = str + p
+  document.getElementById("percent").innerHTML = p2 + "%";
+  document.getElementById("name").innerHTML = "Get 1B depth for multipliers!";
+ }
 };
 setInterval(called, 1)
